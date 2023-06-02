a1091d03c12518a25700efd0ea11036fda8a2572
MariiaS <sirotkinamariya11@gmail.com>
2023-06-02 20:57:17 +0000

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import hotBG from './assets/hot.jpg'
+import coldBG from './assets/cold.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
